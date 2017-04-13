e14780414cc26fc522da3b4fc94f1e25424414b2
servi <servi@169.254.107.100>
2017-04-13 12:27:28 +0300

    Add 1!

--- Git-test-project/src/md/freemanchem/gittest/Main.java
+++ Git-test-project/src/md/freemanchem/gittest/Main.java
@@ -2,9 +2,13 @@ package md.freemanchem.gittest;
 
 public class Main {
 
+	static int a;
 	public static void main(String[] args) {
 		System.out.println("Hello from Git Hub!");
 		System.out.println("Another Hello !!!");
+		System.out.println("Another another Hello !!!");
+		a=1;
+		System.out.println(a);
 	}
 
 }
