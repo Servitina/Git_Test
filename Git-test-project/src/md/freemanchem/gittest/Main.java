e586539da18b647ed1d2b8f65c2bff9c3faca85c
Servitina <freemanchem@yandex.ru>
2017-04-13 11:58:04 +0300

    I have just created new java project

---
+++ Git-test-project/src/md/freemanchem/gittest/Main.java
@@ -0,0 +1,10 @@
+package md.freemanchem.gittest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from Git Hub!");
+		System.out.println("Another Hello !!!");
+	}
+
+}
